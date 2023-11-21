1a807ef3763b3181223c78ab98bcc69ca4665ca6
tientran97 <tien.minhtran615034@gmail.com>
2023-11-21 21:03:01 +0700

    django project set up

---
+++ api/settings/production.py
@@ -0,0 +1,8 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS =[("Tran Tien", "tien.minhtran615034@gmail.com")]
+
+# TODO add domain names of the production server
+
+CSRF_TRUSTED_ORIGINS = [""]
